2183a46c09239f199eac917c429dbb1eebb63055
SaveMoney <carlos.federici929@gmail.com>
2025-04-08 20:47:14 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,21 @@
-// firebase-config.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SUA_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID",
-  measurementId: "SEU_MEASUREMENT_ID"
+  apiKey: "AIzaSyCmw9A3WvecBRr19MhIX5-wKLf66r-voig",
+  authDomain: "savemoney-7b401.firebaseapp.com",
+  projectId: "savemoney-7b401",
+  storageBucket: "savemoney-7b401.firebasestorage.app",
+  messagingSenderId: "1036605042056",
+  appId: "1:1036605042056:web:1d5ee679915dba328c5e3d",
+  measurementId: "G-LX8LY8FF6M"
 };
 
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
