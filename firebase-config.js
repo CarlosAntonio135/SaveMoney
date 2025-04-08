92ab15610f3b7ad80ec08ad1f700d7c63e4e8a54
SaveMoney <carlos.federici929@gmail.com>
2025-04-08 20:45:28 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SUA_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID",
+  measurementId: "SEU_MEASUREMENT_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
