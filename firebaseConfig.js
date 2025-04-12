9a3a34ccb01fe3a9a7234201332dd4e34386a2f2
SaveMoney <carlos.federici929@gmail.com>
2025-04-12 11:39:17 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   projectId: "savemoney-7b401",
   storageBucket: "savemoney-7b401.appspot.com",
   messagingSenderId: "1036605042056",
-  appId: "1:1036605042056:web:SEU_APP_ID_AQUI" // Substitua pelo seu appId real
+  appId: "1:1036605042056:web:1:1036605042056:web:1d5ee679915dba328c5e3d" 
 };
 
 const app = initializeApp(firebaseConfig);
