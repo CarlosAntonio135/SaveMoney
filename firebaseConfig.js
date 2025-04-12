5aa8a50355c383e115d9f7081090010cb4e66e36
SaveMoney <carlos.federici929@gmail.com>
2025-04-12 11:54:32 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmw9A3WwecBRr19MhIX5-wKLf66r-voig",
+  apiKey: "AIzaSyCmw9A3WvecBRr19MhIX5-wKLf66r-voig",
   authDomain: "savemoney-7b401.firebaseapp.com",
   projectId: "savemoney-7b401",
   storageBucket: "savemoney-7b401.appspot.com",
   messagingSenderId: "1036605042056",
-  appId: "1:1036605042056:web:1d5ee679915dba328c5e3d" // ✅ corrigido aqui
+  appId: "1:1036605042056:web:1d5ee679915dba328c5e3d"
 };
 
 const app = initializeApp(firebaseConfig);
