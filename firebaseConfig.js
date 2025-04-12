1e903341e05d9f3a91e537d7e2ebf5b275a5e2b3
SaveMoney <carlos.federici929@gmail.com>
2025-04-12 11:36:35 -0300

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmw9A3WwecBRr19MhIX5-wKLf66r-voig",
+  authDomain: "savemoney-7b401.firebaseapp.com",
+  projectId: "savemoney-7b401",
+  storageBucket: "savemoney-7b401.appspot.com",
+  messagingSenderId: "1036605042056",
+  appId: "1:1036605042056:web:SEU_APP_ID_AQUI" // Substitua pelo seu appId real
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
